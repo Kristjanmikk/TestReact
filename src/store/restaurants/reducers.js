7b73c4d2a4692e43eec19f97ed31f41c8cecadf8
Kristjanmikk <Kristjanmikk@hotmail.com>
2021-05-07 05:50:01 +0300

    edge cases

--- src/store/restaurants/reducers.js
+++ src/store/restaurants/reducers.js
@@ -1,15 +1,27 @@
 import {combineReducers} from 'redux';
-import {STORE_RESTAURANTS} from './actions';
+import {START_LOADING, STORE_RESTAURANTS} from './actions';
 
 const records = (state = [], action) => {
-      switch (action.type) {
-        case STORE_RESTAURANTS:
-          return action.records;
-        default:
-          return state;
-      }
-    };
+    switch (action.type) {
+      case STORE_RESTAURANTS:
+        return action.records;
+      default:
+        return state;
+    }
+  };
+  
+  const loading = (state = false, action) => {
+    switch (action.type) {
+      case START_LOADING:
+        return true;
+      case STORE_RESTAURANTS:
+        return false;
+      default:
+        return state;
+    }
+  };
 
 export default combineReducers({
   records,
+  loading,
 });
